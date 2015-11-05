a6c3cde30402c36f618db21443e9ea3f45c120a3
bldesign <bensonlam@BL-MBA.local>
2015-11-06 07:37:59 +1100

    work on assignment week5

---
+++ Assignment/starter_code/js/app.js
@@ -0,0 +1,11 @@
+//var city = ['austin.jpg','la.jpg','nyc.jpg','sf.jpg','sydney.jpg'];
+var city;
+
+$(document).ready(function(){
+	
+	$("form").submit(function(event){
+		event.preventDefault();
+		$('body').css('background-color','black');
+	});
+	
+})
